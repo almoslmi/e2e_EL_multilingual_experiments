70fabe7e512b9d4a8def78f0e7940a813bb08f99
root <shun.sugiyama@jp21.com>
2019-06-13 01:54:04 +0000

    update eval scripts

--- eval/print_clsreport2.py
+++ eval/print_clsreport2.py
@@ -2,8 +2,7 @@ import json
 from tqdm import tqdm
 
 if __name__ == "__main__":
-    import sys
-    with open("./result.json") as f:
+    with open("./result2.json") as f:
         data = json.load(f)
     recalls = []
     precisions = []
@@ -13,6 +12,7 @@ if __name__ == "__main__":
         pred_in_true = 0
         if len(sent["pred"]) == 0 or len(sent["true"]) == 0:
             continue
+        sent["pred"] = [x[0] for x in sent["pred"]]
         for e in sent["true"]:
             if e in sent["pred"]:
                 true_in_pred += 1
@@ -23,8 +23,8 @@ if __name__ == "__main__":
         recalls.append(float(pred_in_true)/float(len(sent["true"])))
     recall = sum(recalls)/len(recalls)
     precision = sum(precisions)/len(precisions)
-    #f1 = (2 * precision * recall) / (precision + recall)
+    f1 = (2 * precision * recall) / (precision + recall)
            
     print("recall:", recall)
     print("precision:", precision)
-    #print("f1:", f1)
+    print("f1:", f1)
