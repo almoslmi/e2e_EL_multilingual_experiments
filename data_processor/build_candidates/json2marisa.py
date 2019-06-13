dd27247159dde448b4cab182d91f00d073e09024
root <shun.sugiyama@jp21.com>
2019-06-13 05:58:27 +0000

    update processor

--- data_processor/build_candidates/json2marisa.py
+++ data_processor/build_candidates/json2marisa.py
@@ -3,22 +3,13 @@ import sys
 from marisa_trie import BytesTrie
 
 if __name__ == "__main__":
-    print("load types")
-    with open("./out.json") as f:
-        data = json.load(f)
-
-    print("types to trie")
-    trie = BytesTrie([(k,bytes(json.dumps(v), "utf-8")) for k,v in data.items()])
-
-    print("saving...")
-    trie.save("types.marisa")
-
     print("load mention_stat")
     with open("./mention_stat.json") as f:
         data = json.load(f)
 
     print("mention_stat to trie")
-    trie = BytesTrie([(k,bytes(json.dumps(v), "utf-8")) for k,v in data.items()])
+    trie = BytesTrie([(k, bytes(json.dumps(v), "utf-8"))
+                      for k, v in data.items()])
 
     print("saving...")
     trie.save("mention_stat.marisa")
